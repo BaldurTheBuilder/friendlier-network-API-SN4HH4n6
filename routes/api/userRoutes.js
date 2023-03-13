2c586be1dede0a324449d7b2179826ed7ab77597
BaldurTheBuilder <jack.kokcarlson@gmail.com>
2023-03-12 19:09:35 -0700

    pseudocode instructions

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,14 @@
+// get on all users
+// get on single user by _id.
+    // populated w/ thought and friend data
+// post on new user
+// put to update user by _id
+// delete to remove user by _id
+// bonus: remove associated thoughts when deleted
+
+// example: 
+//   {
+//     "username": "lernantino",
+//     "email": "lernantino@gmail.com"
+//   }
+
