224005ea42ef6522f4032664e73dd1379d865b7a
BaldurTheBuilder <jack.kokcarlson@gmail.com>
2023-03-12 18:59:59 -0700

    basic server setup

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-// AS A social media startup
-// I WANT an API for my social network that uses a NoSQL database
-// SO THAT my website can handle large amounts of unstructured data
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
 
-// GIVEN a social network API
-// WHEN I enter the command to invoke the application
-// THEN my server is started and the Mongoose models are synced to the MongoDB database
-// WHEN I open API GET routes in Insomnia for users and thoughts
-// THEN the data for each of these routes is displayed in a formatted JSON
-// WHEN I test API POST, PUT, and DELETE routes in Insomnia
-// THEN I am able to successfully create, update, and delete users and thoughts in my database
-// WHEN I test API POST and DELETE routes in Insomnia
-// THEN I am able to successfully create and delete reactions to thoughts and add and remove friends to a user’s friend list
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// add back in to allow routes
+// app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}!`);
+  });
+});
+
+// WHEN I enter the command to invoke the application the server is started and mongoose models are synced w/ the mongoDB database
+// WHEN I open API GET routes in Insomnia for users and thoughts the data for each routes is displayed in formatted JSON
+// WHEN I test API POST, PUT, and DELETE routes in Insomnia, CRUD functions for users work.
+// WHEN I test API POST and DELETE routes in Insomnia, Create and Delete reactions from thoughts, and Create friends on a friend list
+// revisit: reaction and thought models. Verify the getTimestamp methods are functioning. Verify how they're supposed to be formatted.
